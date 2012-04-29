f97797226c21e64ceab02536f265bd8cf09050f9
ukqida@gmail.com <ukqida@gmail.com@b47bfb7c-4a82-c14d-e78d-40c56cbf1bf0>
2012-04-29 15:16:33 +0000

    add tag for 1.0.0-rc1

---
+++ tags/1.0.0-rc1/info/psidev/psi/pi/mzquantml/io/MzQuantMLMarshaller.java
@@ -0,0 +1,75 @@
+/**
+ * JAXB-based Marshaller for a mzQuantML file.
+ */
+package info.psidev.psi.pi.mzquantml.io;
+
+import info.psidev.psi.pi.mzquantml._1_0.MzQuantMLType;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+
+/**
+ * Class for writing out whole mzQuantML objects.
+ * @author Gerhard Mayer, MPC, Ruhr-University of Bochum
+ */
+public class MzQuantMLMarshaller {
+
+    /**
+     * Members.
+     */
+    private Marshaller marsh = null;
+    private Writer fw = null;
+
+    /**
+     * Constructor.
+     * @param fullFileName
+     */
+    public MzQuantMLMarshaller(String fullFileName) {
+        try {
+            this.fw = new FileWriter(fullFileName);
+        }
+        catch (IOException ioex) {
+            ioex.printStackTrace(System.err);            
+        }
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(MzQuantMLType.class);
+            this.marsh = context.createMarshaller();
+
+            this.marsh.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            this.marsh.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+            // Set JAXB_FRAGMENT_PROPERTY to true for all objects that do not have a @XmlRootElement annotation
+            //this.marsh.setProperty(Marshaller.JAXB_FRAGMENT, true);
+        }
+        catch (JAXBException jaxbex) {
+            jaxbex.printStackTrace(System.err);
+        }
+    }
+
+    /**
+     * Marshalling of a whole MzQuantML object.
+     * @param mzQuantML
+     */
+    public void marshall(MzQuantMLType mzQuantML) {
+        try {
+            if (this.fw != null) {
+                JAXBElement<MzQuantMLType> jaxbElement = new JAXBElement<MzQuantMLType>(new QName("http://psidev.info/psi/pi/mzQuantML/1.0.0-rc1", "MzQuantML"), MzQuantMLType.class, mzQuantML);
+                this.marsh.marshal(jaxbElement, this.fw);
+                //this.marsh.marshal(mzQuantML, this.fw);
+                this.fw.flush();
+                this.fw.close();
+            }
+        }
+        catch (JAXBException jaxbex) {
+            jaxbex.printStackTrace(System.err);
+        }
+        catch (IOException ioex) {
+            ioex.printStackTrace(System.err);
+        }
+    }
+}
---
+++ tags/1.0.0-rc1/info/psidev/psi/pi/mzquantml/io/MzQuantMLUnmarshaller.java
@@ -0,0 +1,72 @@
+/**
+ * JAXB-based Marshaller for a mzQuantML file.
+ */
+package info.psidev.psi.pi.mzquantml.io;
+
+import info.psidev.psi.pi.mzquantml._1_0.MzQuantMLType;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.helpers.DefaultValidationEventHandler;
+
+/**
+ * Class for reading in whole mzQuantML objects.
+ * @author Gerhard Mayer, MPC, Ruhr-University of Bochum
+ */
+public class MzQuantMLUnmarshaller {
+
+    /**
+     * Members.
+     */
+    private Unmarshaller unmarsh = null;
+    private Reader fr = null;
+
+    /**
+     * Constructor
+     * @param fullFileName
+     */
+    public MzQuantMLUnmarshaller(String fullFileName) {
+        try {
+            this.fr = new FileReader(fullFileName);
+        }
+        catch (IOException ioex) {
+            ioex.printStackTrace(System.err);            
+        }
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(new Class[] { MzQuantMLType.class});
+            this.unmarsh = context.createUnmarshaller();
+            this.unmarsh.setEventHandler(new DefaultValidationEventHandler());
+            //this.unmarsh.setValidating(true);
+        }
+        catch (JAXBException jaxbex) {
+            jaxbex.printStackTrace(System.err);
+        }
+    }
+
+    /**
+     * Unmarshalling of a whole MzQuantML object.
+     * @return MzQuantMLType
+     */
+    public MzQuantMLType unmarshall() {
+        MzQuantMLType mzQuantML = null;
+
+        try {
+            if (this.fr != null) {
+                mzQuantML = (MzQuantMLType) this.unmarsh.unmarshal(this.fr);
+                this.fr.close();
+            }
+        }
+        catch (JAXBException jaxbex) {
+            jaxbex.printStackTrace(System.err);
+        }
+        catch (IOException ioex) {
+            ioex.printStackTrace(System.err);
+        }
+
+        return mzQuantML;
+    }
+}
