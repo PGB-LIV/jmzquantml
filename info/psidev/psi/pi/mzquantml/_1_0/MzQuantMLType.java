c3899558452e48b3f97cb3f396729b881c36ab97
ukqida <ukqida@gmail.com>
2012-05-04 13:23:18 +0000

    add @XmlRootElement(name="MzQuantML") to MzQuantMLType to fix the unmarshaller error: javax.xml.bind.UnmarshalException: unexpected element

--- info/psidev/psi/pi/mzquantml/_1_0/MzQuantMLType.java
+++ info/psidev/psi/pi/mzquantml/_1_0/MzQuantMLType.java
@@ -14,6 +14,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -76,6 +77,7 @@ import javax.xml.datatype.XMLGregorianCalendar;
     "smallMoleculeList",
     "featureList"
 })
+@XmlRootElement(name="MzQuantML")
 public class MzQuantMLType {
 
     @XmlElement(name = "CvList", required = true)
