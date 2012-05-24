3157a65dcf1c3b1dd6cb7c5f662dec37697a0abc
ukqida <ukqida@gmail.com>
2012-05-24 10:46:35 +0000

    updated to the latest mzQuantML_1_0_0-rc2 version

--- info/psidev/psi/pi/mzquantml/io/MzQuantMLUnmarshaller.java
+++ info/psidev/psi/pi/mzquantml/io/MzQuantMLUnmarshaller.java
@@ -4,6 +4,7 @@
 package info.psidev.psi.pi.mzquantml.io;
 
 import info.psidev.psi.pi.mzquantml._1_0.MzQuantMLType;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -11,9 +12,12 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.helpers.DefaultValidationEventHandler;
+//import uk.ac.liv.jmzquantml.xml.xxindex.MzQuantMLIndexer;
+//import uk.ac.liv.jmzquantml.xml.xxindex.MzQuantMLIndexerFactory;
 
 /**
  * Class for reading in whole mzQuantML objects.
+ *
  * @author Gerhard Mayer, MPC, Ruhr-University of Bochum
  */
 public class MzQuantMLUnmarshaller {
@@ -23,32 +27,36 @@ public class MzQuantMLUnmarshaller {
      */
     private Unmarshaller unmarsh = null;
     private Reader fr = null;
+    //private final MzQuantMLIndexer index;
 
     /**
      * Constructor
+     *
      * @param fullFileName
      */
     public MzQuantMLUnmarshaller(String fullFileName) {
+
+        //this(MzQuantMLIndexerFactory.getInstance().buildIndex(new File(fullFileName)));
+
         try {
             this.fr = new FileReader(fullFileName);
-        }
-        catch (IOException ioex) {
-            ioex.printStackTrace(System.err);            
+        } catch (IOException ioex) {
+            ioex.printStackTrace(System.err);
         }
 
         try {
-            JAXBContext context = JAXBContext.newInstance(new Class[] { MzQuantMLType.class});
+            JAXBContext context = JAXBContext.newInstance(new Class[]{MzQuantMLType.class});
             this.unmarsh = context.createUnmarshaller();
             this.unmarsh.setEventHandler(new DefaultValidationEventHandler());
             //this.unmarsh.setValidating(true);
-        }
-        catch (JAXBException jaxbex) {
+        } catch (JAXBException jaxbex) {
             jaxbex.printStackTrace(System.err);
         }
     }
 
     /**
      * Unmarshalling of a whole MzQuantML object.
+     *
      * @return MzQuantMLType
      */
     public MzQuantMLType unmarshall() {
@@ -59,14 +67,39 @@ public class MzQuantMLUnmarshaller {
                 mzQuantML = (MzQuantMLType) this.unmarsh.unmarshal(this.fr);
                 this.fr.close();
             }
-        }
-        catch (JAXBException jaxbex) {
+        } catch (JAXBException jaxbex) {
             jaxbex.printStackTrace(System.err);
-        }
-        catch (IOException ioex) {
+        } catch (IOException ioex) {
             ioex.printStackTrace(System.err);
         }
 
         return mzQuantML;
     }
+
+    /*
+     * Constructor
+     */
+//    public MzQuantMLUnmarshaller(File mzQuantMLFile) {
+//        this(MzQuantMLIndexerFactory.getInstance().buildIndex(mzQuantMLFile));
+//    }
+//
+//    public MzQuantMLUnmarshaller(MzQuantMLIndexer indexer) {
+//        this.index = indexer;
+//    }
+    /*
+     * public methods
+     */
+
+    /**
+     * @param xpath the xpath defining the XML element.
+     * @return the number of XML elements matching the xpath or -1 if no
+     * elements were found for the specified xpath.
+     */
+//    public int getObjectCountForXpath(String xpath) {
+//        if (xpath != null) {
+//            return index.getCount(xpath);
+//        } else {
+//            return -1;
+//        }
+//    }
 }
