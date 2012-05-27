2ef20f23e56a1076814ee901795ef654c4c2d864
ukqida@gmail.com <ukqida@gmail.com@b47bfb7c-4a82-c14d-e78d-40c56cbf1bf0>
2012-05-27 22:54:42 +0000

    update MzQuantMLType.java

--- trunk/info/psidev/psi/pi/mzquantml/_1_0/MzQuantMLType.java
+++ trunk/info/psidev/psi/pi/mzquantml/_1_0/MzQuantMLType.java
@@ -10,12 +10,7 @@ package info.psidev.psi.pi.mzquantml._1_0;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 
