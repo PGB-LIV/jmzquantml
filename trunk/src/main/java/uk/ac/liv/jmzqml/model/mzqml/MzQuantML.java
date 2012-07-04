d483e3842429d2e2cfd0cd4b0c3ca9d78eb8e64a
ukqida@gmail.com <ukqida@gmail.com@b47bfb7c-4a82-c14d-e78d-40c56cbf1bf0>
2012-07-04 01:11:43 +0000

    commit before release prepare

--- trunk/src/main/java/uk/ac/liv/jmzqml/model/mzqml/MzQuantML.java
+++ trunk/src/main/java/uk/ac/liv/jmzqml/model/mzqml/MzQuantML.java
@@ -5,12 +5,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import uk.ac.liv.jmzqml.model.MzQuantMLObject;
 import uk.ac.liv.jmzqml.xml.jaxb.adapter.CalendarAdapter;
@@ -73,6 +68,7 @@ import uk.ac.liv.jmzqml.xml.jaxb.adapter.CalendarAdapter;
     "smallMoleculeList",
     "featureList"
 })
+@XmlRootElement(name="MzQuantML")
 public class MzQuantML
     implements Serializable, MzQuantMLObject
 {
