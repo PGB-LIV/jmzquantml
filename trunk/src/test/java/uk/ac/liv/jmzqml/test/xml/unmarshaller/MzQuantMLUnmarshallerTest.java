31edc25fa2e55bd11ead6b3376fad35459d6d379
ukqida@gmail.com <ukqida@gmail.com@b47bfb7c-4a82-c14d-e78d-40c56cbf1bf0>
2013-03-07 15:56:34 +0000

    fixed some bugs for MzQuantMLUnmarshaller but not completed

--- trunk/src/test/java/uk/ac/liv/jmzqml/test/xml/unmarshaller/MzQuantMLUnmarshallerTest.java
+++ trunk/src/test/java/uk/ac/liv/jmzqml/test/xml/unmarshaller/MzQuantMLUnmarshallerTest.java
@@ -17,25 +17,106 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.ac.liv.jmzqml.test.xml.unmarshaller;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import uk.ac.liv.jmzqml.MzQuantMLElement;
 import uk.ac.liv.jmzqml.xml.io.MzQuantMLUnmarshaller;
+
+import static org.junit.Assert.*;
+import uk.ac.liv.jmzqml.model.mzqml.*;
 
 /**
  *
- * @author Da Qi @institute University of Liverpool @time 10-Jul-2012 11:37:50
+ * @author Da Qi
+ * @institute University of Liverpool
+ * @time 10-Jul-2012 11:37:50
  */
 public class MzQuantMLUnmarshallerTest {
 
     private static MzQuantMLUnmarshaller unmarshaller;
 
     static {
-        String fileName = "mzQuantML_example_SILAC_OpenMS-SILACAnalyzer.mzq";
+        String fileName = "CPTAC-Progenesis-small-example.mzq";
         URL xmlFileURL = MzQuantMLUnmarshallerTest.class.getClassLoader().getResource(fileName);
         assertNotNull(xmlFileURL);
 
-        //unmarshaller = new MzQuantMLUnmarshaller;
+        unmarshaller = new MzQuantMLUnmarshaller(xmlFileURL);
+        assertNotNull(unmarshaller);
     }
+
+    @Test
+    public void testAttributeRetrieval()
+            throws Exception {
+
+        // Number of software
+        int total = unmarshaller.getObjectCountForXpath(MzQuantMLElement.Software.getXpath());
+        assertEquals(1, total);
+
+        Software software = unmarshaller.unmarshal(MzQuantMLElement.Software);
+        assertNotNull(software);
+        String id = "Progenesis";
+        Map<String, String> attributes = unmarshaller.getElementAttributes(id, MzQuantMLElement.Software.getClazz());
+        assertNotNull(attributes);
+        assertEquals(id, attributes.get("id"));
+
+        assertEquals(2, attributes.keySet().size());
+        assertEquals("2.3", attributes.get("version"));
+
+        // test cv 
+        Cv cv = unmarshaller.unmarshal(MzQuantMLElement.Cv);
+        assertNotNull(cv);
+
+        id = "PSI-MS";
+        attributes = unmarshaller.getElementAttributes(id, MzQuantMLElement.Cv.getClazz());
+        assertNotNull(attributes);
+        assertEquals(id, attributes.get("id"));
+
+        assertEquals(4, attributes.keySet().size());
+        assertEquals("Proteomics Standards Initiative Mass Spectrometry Vocabularies", attributes.get("fullName"));
+        assertEquals("http://psidev.cvs.sourceforge.net/viewvc/*checkout*/psidev/psi/psi-ms/mzML/controlledVocabulary/psi-ms.obo", attributes.get("uri"));
+        assertEquals("2.25.0", attributes.get("version"));
+    }
+
+    @Test
+    public void testRootAttributeRetrieval()
+            throws Exception {
+        // check the root element (mzQuantML) attributes
+        assertEquals("Progenesis-Label-Free-26112012", unmarshaller.getMzQuantMLId());
+        assertEquals("1.0.0", unmarshaller.getMzQuantMLVersion());
+        assertNull(unmarshaller.getMzQuantMLName());
+
+        String id = "Progenesis-Label-Free-26112012";
+        Map<String, String> attributes = unmarshaller.getElementAttributes(id, MzQuantMLElement.MzQuantML.getClazz());
+        assertNotNull(attributes);
+        assertEquals(6, attributes.keySet().size());
+        assertEquals("http://www.w3.org/2001/XMLSchema-instance", attributes.get("xmlns:xsi"));
+        assertEquals("http://psidev.info/psi/pi/mzQuantML/1.0.0 ../../../schema/mzQuantML_1_0_0.xsd", attributes.get("xsi:schemaLocation"));
+        assertEquals("http://psidev.info/psi/pi/mzQuantML/1.0.0", attributes.get("xmlns"));
+        assertEquals("2012-11-26T13:10:55.529Z", attributes.get("creationDate"));
+        assertEquals("1.0.0", attributes.get("version"));
+        assertEquals(id, attributes.get("id"));
+    }
+
+    @Test
+    public void testAuditCollection()
+            throws Exception {
+        AuditCollection ac = unmarshaller.unmarshal(MzQuantMLElement.AuditCollection);
+        assertNotNull(ac);
+    }
+
+    @Test
+    public void testAssayList()
+            throws Exception {
+        AssayList assayList = unmarshaller.unmarshal(MzQuantMLElement.AssayList);
+        assertNotNull(assayList);
+        List<Assay> assays = assayList.getAssay();
+        assertNotNull(assays);
+    }
+
 }
