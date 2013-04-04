4737d040ba1338accd2a01ddb3d05e0ce7d3aa35
ukqida@gmail.com <ukqida@gmail.com@b47bfb7c-4a82-c14d-e78d-40c56cbf1bf0>
2013-04-04 12:58:07 +0000

    update MzQuantMLMarshallerTest

--- trunk/src/test/java/uk/ac/liv/jmzqml/test/xml/marshaller/MzQuantMLMarshallerTest.java
+++ trunk/src/test/java/uk/ac/liv/jmzqml/test/xml/marshaller/MzQuantMLMarshallerTest.java
@@ -20,15 +20,16 @@
 
 package uk.ac.liv.jmzqml.test.xml.marshaller;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import uk.ac.liv.jmzqml.MzQuantMLElement;
-import uk.ac.liv.jmzqml.model.mzqml.AnalysisSummary;
-import uk.ac.liv.jmzqml.model.mzqml.AuditCollection;
-import uk.ac.liv.jmzqml.model.mzqml.CvList;
+import uk.ac.liv.jmzqml.model.mzqml.*;
 import uk.ac.liv.jmzqml.xml.io.MzQuantMLMarshaller;
 import uk.ac.liv.jmzqml.xml.io.MzQuantMLUnmarshaller;
 
@@ -48,6 +49,11 @@ public class MzQuantMLMarshallerTest {
         int personCount = -1;
         int orgCount = -1;
         int analSumCount = -1;
+        int swCount = -1;
+        int dpCount = -1;
+        int assCount = -1;
+        int svCount = -1;
+        int protCount = -1;
 
         URL xmlFileURL = MzQuantMLMarshallerTest.class.getClassLoader().getResource("CPTAC-Progenesis-small-example.mzq");
         assertNotNull(xmlFileURL);
@@ -105,12 +111,100 @@ public class MzQuantMLMarshallerTest {
             analSumCount = analSum.getParamGroup().size();
             m.marshall(analSum, writer);
             writer.write("\n");
+
+            InputFiles inFiles = unmarshaller.unmarshal(MzQuantMLElement.InputFiles.getXpath());
+            m.marshall(inFiles, writer);
+            writer.write("\n");
+
+            SoftwareList swList = unmarshaller.unmarshal(MzQuantMLElement.SoftwareList.getXpath());
+            swCount = swList.getSoftware().size();
+            m.marshall(swList, writer);
+
+            DataProcessingList dpList = unmarshaller.unmarshal(MzQuantMLElement.DataProcessingList.getXpath());
+            dpCount = dpList.getDataProcessing().size();
+            m.marshall(dpList, writer);
+
+            Iterator<BibliographicReference> bibliRefIter = unmarshaller.unmarshalCollectionFromXpath(MzQuantMLElement.BibliographicReference);
+            while (bibliRefIter.hasNext()) {
+                BibliographicReference bibliRef = bibliRefIter.next();
+                m.marshall(bibliRef, writer);
+                writer.write("\n");
+            }
+
+            AssayList assList = unmarshaller.unmarshal(MzQuantMLElement.AssayList.getXpath());
+            assCount = assList.getAssay().size();
+            m.marshall(assList, writer);
+            writer.write("\n");
+
+            StudyVariableList svList = unmarshaller.unmarshal(MzQuantMLElement.StudyVariableList.getXpath());
+            svCount = svList.getStudyVariable().size();
+            m.marshall(svList, writer);
+            writer.write("\n");
+
+            ProteinList protList = unmarshaller.unmarshal(MzQuantMLElement.ProteinList.getXpath());
+            protCount = protList.getProtein().size();
+            m.marshall(protList, writer);
+            writer.write("\n");
+
+            Iterator<PeptideConsensusList> pepConListIter = unmarshaller.unmarshalCollectionFromXpath(MzQuantMLElement.PeptideConsensusList);
+            while (pepConListIter.hasNext()) {
+                PeptideConsensusList pepConList = pepConListIter.next();
+                m.marshall(pepConList, writer);
+                writer.write("\n");
+            }
+
+            Iterator<FeatureList> ftListIter = unmarshaller.unmarshalCollectionFromXpath(MzQuantMLElement.FeatureList);
+            while (ftListIter.hasNext()) {
+                FeatureList ftList = ftListIter.next();
+                m.marshall(ftList, writer);
+                writer.write("\n");
+            }
+
+            writer.write(m.createMzQuantMLClosingTag());
         }
         finally {
             if (writer != null) {
                 writer.close();
             }
         }
+
+        File outputFile = new File("output.xml");
+        unmarshaller = new MzQuantMLUnmarshaller(outputFile);
+        MzQuantML mzq = unmarshaller.unmarshal(MzQuantMLElement.MzQuantML);
+        /**
+         * Do some shallow testing. Confirm correct number of child elements returned for each tested element.
+         */
+        assertTrue(mzq.getId().equals("12345"));
+        assertTrue(cvCount >= 0);
+        assertTrue(mzq.getCvList().getCv().size() == cvCount);
+        assertTrue(personCount >= 0);
+        assertTrue(mzq.getAuditCollection().getPerson().size() == personCount);
+        assertTrue(orgCount >= 0);
+        assertTrue(mzq.getAuditCollection().getOrganization().size() == orgCount);
+        assertTrue(analSumCount > 0);
+        assertTrue(mzq.getAnalysisSummary().getParamGroup().size() == analSumCount);
+        assertTrue(swCount >= 0);
+        assertTrue(mzq.getSoftwareList().getSoftware().size() == swCount);
+        assertTrue(dpCount >= 0);
+        assertTrue(mzq.getDataProcessingList().getDataProcessing().size() == dpCount);
+        assertTrue(assCount >= 0);
+        assertTrue(mzq.getAssayList().getAssay().size() == assCount);
+        assertTrue(svCount >= 0);
+        assertTrue(mzq.getStudyVariableList().getStudyVariable().size() == svCount);
+        assertTrue(protCount >= 0);
+        assertTrue(mzq.getProteinList().getProtein().size() == protCount);
+        assertTrue(mzq.getBibliographicReference().get(0).getAuthors().equals("D. Qi, P. Brownridge, D. Xia, K. Mackay, F. F. Gonzalez-Galarza, J. Kenyani, V. Harman, R. J. Beynon and A. R. Jones"));
+        assertTrue(mzq.getBibliographicReference().get(0).getId().equals("BF_DQ1"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMarshallEmptyAuditCollection()
+            throws Exception {
+        URL xmlFileURL = MzQuantMLMarshallerTest.class.getClassLoader().getResource("CPTAC-Progenesis-small-example.mzq");
+        assertNotNull(xmlFileURL);
+        MzQuantMLUnmarshaller unmarshaller = new MzQuantMLUnmarshaller(xmlFileURL);
+        assertNotNull(unmarshaller);
+        MzQuantMLMarshaller m = new MzQuantMLMarshaller();
     }
 
 }
