696b8617b96b06c08f73cca891992b93876452e8
ukqida@gmail.com <ukqida@gmail.com@b47bfb7c-4a82-c14d-e78d-40c56cbf1bf0>
2013-05-07 13:11:17 +0000

    final uploads before 1.0.0-1.0.1 release

--- trunk/src/test/java/uk/ac/liv/jmzqml/test/xml/marshaller/MzQuantMLMarshallerTest.java
+++ trunk/src/test/java/uk/ac/liv/jmzqml/test/xml/marshaller/MzQuantMLMarshallerTest.java
@@ -25,7 +25,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Iterator;
-import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import uk.ac.liv.jmzqml.MzQuantMLElement;
@@ -189,8 +188,8 @@ public class MzQuantMLMarshallerTest {
         assertTrue(mzq.getAuditCollection().getPerson().size() == personCount);
         assertTrue(orgCount >= 0);
         assertTrue(mzq.getAuditCollection().getOrganization().size() == orgCount);
-//        assertTrue(analSumCount > 0);
-//        assertTrue(mzq.getAnalysisSummary().getParamGroup().size() == analSumCount);
+        assertTrue(analSumCount > 0);
+        assertTrue(mzq.getAnalysisSummary().getParamGroup().size() == analSumCount);
         assertTrue(swCount >= 0);
         assertTrue(mzq.getSoftwareList().getSoftware().size() == swCount);
         assertTrue(dpCount >= 0);
